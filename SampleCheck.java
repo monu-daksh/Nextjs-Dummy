c086e03c99e3be428986a79c7cab4a1cefc2b97e
Monu MONU <mmonu@axway.com>
2026-03-25 12:04:56 +0530

    makes changes in new_

--- SampleCheck.java
+++ SampleCheck.java
@@ -7,4 +7,12 @@ public class BadSmallJava {
             System.out.println("Bad Java Code");
         }
     }
+       public static void main(String[] args) {
+        int x = 5;
+        int y = 0;
+        System.out.println(x / y); // ❌ division by zero
+        if (true) { // ❌ always true
+            System.out.println("Bad Java Code");
+        }
+    }
 }
--- sample_check.py
+++ sample_check.py
@@ -1,5 +1,8 @@
 def main():
     value = None
+    value = None
+    value = None
+
     if value == None:  #  bad None check
         print("Bad Python Code")
     print(10 / 0)  # ❌ division by zero
